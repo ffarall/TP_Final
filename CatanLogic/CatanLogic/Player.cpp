900888f9e2afbd70cbed3993efdc84fbb170d88d
facundofarall <33322493+facundofarall@users.noreply.github.com>
2018-11-28 23:38:49 -0300

    poniendo en comun

--- CatanLogic/CatanLogic/Player.cpp
+++ CatanLogic/CatanLogic/Player.cpp
@@ -26,3 +26,20 @@ size_t Player::getVictoryPoints()
 {
 	return victoryPoints;
 }
+
+void Player::allJunctionsAvailable()
+{
+	availableForSettlements = { "0A", "0B", "10C", 
+								"A05", "B0A", "C0B", "1C",
+								"A5D", "BA3", "CBF", "1CG",
+								"D5", "EAD", "FBE", "GCF", "1G",
+								"D5H", "EDI", "FEJ", "GFK", "1GL",
+								"H5", "IDH", "JEI", "KFG", "LGK", "1L2",
+								"H54", "IHM", "JIN", "KJO", "LKP", "2L",
+								"MH4", "NIM", "OJN", "PKO", "2LP",
+								"M4", "NMQ", "ONR", "POS", "2P",
+								"QM4", "RNQ", "SOR", "2PS",
+								"Q4", "RQ3", "SR3", "2S3",
+								"3Q4", "3R", "3S"
+	};
+}
--- CatanLogic/CatanLogic/Player.h
+++ CatanLogic/CatanLogic/Player.h
@@ -3,6 +3,7 @@
 #include <map>
 #include <list>
 #include <string>
+#include <vector>
 #include "Settlement.h"
 #include "Road.h"
 
@@ -44,5 +45,27 @@ private:
 
 	// Sets all corners of board available for building Settlements.
 	void allJunctionsAvailable();
+
+	// All combinations of edges.
+	const vector< const string > allEdges = {
+		"A05", "B0A", "0BC", "C0", "1C0",
+		"A5", "BA", "CB", "1CG",
+		"D5A", "AD"
+	};
+	// All combinations for vertexes.
+	const vector< const string > allVertexes = { 
+		"0A", "0B", "10C",
+		"A05", "B0A", "C0B", "1C",
+		"A5D", "BA3", "CBF", "1CG",
+		"D5", "EAD", "FBE", "GCF", "1G",
+		"D5H", "EDI", "FEJ", "GFK", "1GL",
+		"H5", "IDH", "JEI", "KFG", "LGK", "1L2",
+		"H54", "IHM", "JIN", "KJO", "LKP", "2L",
+		"MH4", "NIM", "OJN", "PKO", "2LP",
+		"M4", "NMQ", "ONR", "POS", "2P",
+		"QM4", "RNQ", "SOR", "2PS",
+		"Q4", "RQ3", "SR3", "2S3",
+		"3Q4", "3R", "3S"
+	};
 };
 
