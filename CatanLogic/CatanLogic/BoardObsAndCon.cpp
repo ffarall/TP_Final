c2cca887ef559460a1f09c575a489610f8dcd282
facundofarall <33322493+facundofarall@users.noreply.github.com>
2018-12-14 23:00:38 -0300

    reportando antes de salir

--- CatanLogic/CatanLogic/BoardObsAndCon.cpp
+++ CatanLogic/CatanLogic/BoardObsAndCon.cpp
@@ -2,27 +2,36 @@
 #include "BoardController.h"
 
 #define MAP_FILE "mapaFinal.png"
-#define HILLS_HEX_FILE "arcillaOK.png"
-#define WOODS_HEX_FILE "bosqueOK.png"
-#define MOUNTAINS_HEX_FILE "montañaOK.png"
-#define FIELDS_HEX_FILE "campoOK.png"
+#define HILLS_HEX_FILE "colinas.png"
+#define WOODS_HEX_FILE "bosque.png"
+#define MOUNTAINS_HEX_FILE "montaña.png"
+#define FIELDS_HEX_FILE "campo.png"
 #define PASTURES_HEX_FILE "pastura.png"
-#define DESERT_HEX_FILE "desiertoOK.png"
+#define DESERT_HEX_FILE "desierto.png"
 #define SEA_3X1_FILE "sea3x1.png"
 #define SEA_2TX1_FILE "sea2Tx1.png"
 #define SEA_2OX1_FILE "sea2Ox1.png"
 #define SEA_2LX1_FILE "sea2Lx1.png"
 #define SEA_2PX1_FILE "sea2Px1.png"
 #define SEA_2MX1_FILE "sea2Mx1.png"
+#define SETTLEMENT_FILE "settlement.png"
+#define CITY_FILE "city.png"
+#define ROAD_FILE "road.png"
+#define ROBBER_FILE "robber.png"
+
+#define BOARD_POS_X 310
+#define BOARD_POS_Y 88
 
 BoardObsAndCon::BoardObsAndCon()
 {
 	init();
+	update();
 }
 
 
 BoardObsAndCon::~BoardObsAndCon()
 {
+	delete controller;
 	delete decoder;
 }
 
@@ -30,4 +39,17 @@ void BoardObsAndCon::init()
 {
 	controller = new BoardController;
 	decoder = new MapDecoder(MAP_FILE);
+
+	bitmaps[HILLS_HEX_FILE] = al_load_bitmap(HILLS_HEX_FILE);
+	bitmaps[WOODS_HEX_FILE] = al_load_bitmap(WOODS_HEX_FILE);
+	bitmaps[MOUNTAINS_HEX_FILE] = al_load_bitmap(MOUNTAINS_HEX_FILE);
+	bitmaps[FIELDS_HEX_FILE] = al_load_bitmap(FIELDS_HEX_FILE);
+	bitmaps[PASTURES_HEX_FILE] = al_load_bitmap(PASTURES_HEX_FILE);
+	bitmaps[DESERT_HEX_FILE] = al_load_bitmap(DESERT_HEX_FILE);
+	bitmaps[SEA_3X1_FILE] = al_load_bitmap(SEA_3X1_FILE);
+	bitmaps[SEA_2TX1_FILE] = al_load_bitmap(SEA_2TX1_FILE);
+	bitmaps[SEA_2OX1_FILE] = al_load_bitmap(SEA_2OX1_FILE);
+	bitmaps[SEA_2LX1_FILE] = al_load_bitmap(SEA_2LX1_FILE);
+	bitmaps[SEA_2PX1_FILE] = al_load_bitmap(SEA_2PX1_FILE);
+	bitmaps[SEA_2MX1_FILE] = al_load_bitmap(SEA_2MX1_FILE);
 }
