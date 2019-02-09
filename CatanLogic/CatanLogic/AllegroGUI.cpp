f305386327e2fa1f8ca1c088a8a23fc11684f5a7
facundofarall <33322493+facundofarall@users.noreply.github.com>
2019-02-09 16:48:45 -0300

    ethsrt

--- CatanLogic/CatanLogic/AllegroGUI.cpp
+++ CatanLogic/CatanLogic/AllegroGUI.cpp
@@ -5,8 +5,8 @@
 #include"allegro5/allegro_font.h"
 #include "BoardController.h"
 
-#define DIS_WIDTH 1000
-#define DIS_HEIGHT 600
+#define DIS_WIDTH 1200
+#define DIS_HEIGHT 700
 #define TIMER_TIME 100
 
 #define TX(x) (static_cast<void (GUIEnabler::* )()>(&AllegroGUI::x))
--- CatanLogic/CatanLogic/MovableType.cpp
+++ CatanLogic/CatanLogic/MovableType.cpp
@@ -26,7 +26,7 @@ void MovableType::printType()
 {
 	if (bitmap != NULL)
 	{
-		al_draw_bitmap(bitmap, coordinateX, coordinateY, flags);
+		al_draw_bitmap(bitmap, coordinateX, coordinateY, flags);				// AGREGADO
 		al_draw_tinted_scaled_rotated_bitmap(bitmap, tint, coordinateX, coordinateY, difX, difY, scaleX, scaleY, angle, flags);
 	}
 }
