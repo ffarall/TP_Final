34a46531077af0a645e5a7f8aa1d43d063b4b941
Santiago Arribere <sarribere@itba.edu.ar>
2019-02-11 08:18:05 -0300

    dados

--- CatanLogic/CatanLogic/package.cpp
+++ CatanLogic/CatanLogic/package.cpp
@@ -298,8 +298,8 @@ std::string DicePkg::getPackage()
 {
 	std::string ret;
 	ret.push_back(static_cast<char>(nombre));
-	ret.push_back(dice1);
-	ret.push_back(dice2);
+	ret.push_back(dice1+'0');
+	ret.push_back(dice2+'0');
 	return ret;
 }
 
